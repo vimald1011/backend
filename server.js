393f3f01a4363718586a633530c9d1ad6f9ac75e
Vimal Dubey <144519434+vimald1011@users.noreply.github.com>
2025-04-20 22:13:37 +0530

    change the url

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 // app.use(cors());
-app.use(cors({ origin: "http://localhost:4200" }));
+app.use(cors({ origin: "https://www.adarshcodes.com" }));
 
 app.use(express.json());
 

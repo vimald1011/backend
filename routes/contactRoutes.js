a5224604c31bd680c49e59b03a964e3d466e4507
Vimal Dubey <144519434+vimald1011@users.noreply.github.com>
2025-04-23 13:23:02 +0530

    adding email service

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const Contact = require("../models/Contact");
-const { from, Subject } = require("rxjs");
 const nodemailer = require('nodemailer');
 
 
